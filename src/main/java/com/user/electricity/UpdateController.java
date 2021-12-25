680629622edc0e2b218ca00abf5a431a21950adf
a-elrawy <abdelrhman.elrawy1@gmail.com>
2021-12-25 17:35:27 +0200

    edit

--- src/main/java/com/user/electricity/UpdateController.java
+++ src/main/java/com/user/electricity/UpdateController.java
@@ -13,7 +13,6 @@ public class UpdateController extends SwitchingController {
     public static String meterCode;
     public static String path;
     public static int id;
-    public static int tempPassword;
     @FXML
     private TextField name;
 
@@ -31,22 +30,53 @@ public class UpdateController extends SwitchingController {
 
     @FXML
     private Label meterCodeLabel;
-
+    @FXML
+    private  TextField meterCodeF;
+    @FXML
+    private Text error;
     @FXML
     void submit(ActionEvent event) throws IOException, ClassNotFoundException {
         Customer customer = new Customer(id,name.getText(),email.getText(),password.getText(),address.getText(),region.getText(),meterCode,path);
         Customer.update_customer(customer);
     }
     public  void initialize() throws IOException, ClassNotFoundException {
-        meterCodeLabel.setText(meterCode);
-        Customer customer = Customer.read_customer(meterCode);
-        id = customer.id;
-        path = customer.getPathToContract();
-        name.setText(customer.username);
-        address.setText(customer.getAddress());
-        email.setText(customer.getEmail());
-        region.setText(customer.getRegion());
+        try {
+            meterCodeLabel.setText(meterCode);
+            Customer customer = Customer.read_customer(meterCode);
+            id = customer.id;
+            path = customer.getPathToContract();
+            name.setText(customer.username);
+            address.setText(customer.getAddress());
+            email.setText(customer.getEmail());
+            region.setText(customer.getRegion());
+        } catch (Exception e){
+            error.setVisible(false);
+        }
 
+    }
+
+    public void add(ActionEvent e) throws IOException {
+        switchToAdd(e);
+    }
+    public void delete(ActionEvent e) throws IOException, ClassNotFoundException {
+        try {
+            String metercode =meterCodeF.getText();
+            Customer.remove_customer(metercode);
+            error.setVisible(false);
+        }catch (Exception exception){
+            error.setVisible(true);
+        }
+    }
+    public void modify(ActionEvent e) throws IOException {
+        try {
+            String metercode =meterCodeF.getText();
+            Customer.read_customer(metercode);
+            UpdateController.meterCode =meterCodeF.getText();
+            error.setVisible(false);
+            switchToUpdate(e);
+        }catch (Exception exception){
+            error.setVisible(true);
+        }
     }
 
 }
--- src/main/java/com/user/electricity/customerProfileController.java
+++ src/main/java/com/user/electricity/customerProfileController.java
@@ -1,24 +1,19 @@
 package com.user.electricity;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 import java.io.IOException;
 
 public class customerProfileController extends SwitchingController{
-    @FXML
-    private Button Complaint_button;
 
-    @FXML
-    private Button Inbox_button;
 
     @FXML
     private TextField address_p;
-
-    @FXML
-    private Button bills_button;
 
     @FXML
     private TextField customer_name_p;
@@ -30,18 +25,63 @@ public class customerProfileController extends SwitchingController{
     private TextField government_p;
 
     @FXML
-    private Label logout_button;
+    private TextField meter_code_p;
+    @FXML
+    private TextArea comText;
+    private final static double chargeConstant = 5.5;
 
     @FXML
-    private TextField meter_code_p;
+    private TextField address_2;
+
+    @FXML
+    private TextField charges_p;
+
+    @FXML
+    private TextField customer_name_2;
+
+    @FXML
+    private TextField government_2;
+
+    @FXML
+    private TextField MonthlyRead;
+
+    @FXML
+    private TextField real_comsuption_p;
 
     public void initialize() throws IOException, ClassNotFoundException {
         Customer customer = Customer.read_customer(Utilities.CurrentUserID);
         assert customer != null;
-        customer_name_p.setText(customer.username);
-        email_p.setText(customer.getEmail());
-        government_p.setText(customer.getRegion());
-        meter_code_p.setText(customer.getMeterCode());
-        address_p.setText(customer.getAddress());
+        try {
+            customer_name_p.setText(customer.username);
+            email_p.setText(customer.getEmail());
+            government_p.setText(customer.getRegion());
+            meter_code_p.setText(customer.getMeterCode());
+            address_p.setText(customer.getAddress());
+        } catch (Exception ignored){
+
+        }
+
     }
+    public void sendComplaint(ActionEvent event) throws IOException, ClassNotFoundException {
+        Customer customer = Customer.read_customer(Utilities.CurrentUserID);
+        String complaintText = comText.getText();
+        assert customer != null;
+        Complaint complaint = new Complaint(customer.username,customer.getAddress(),customer.getRegion(),complaintText);
+        Utilities.write(complaint,Utilities.ComplaintsFilename);
+        switchToCustomerProfile(event);
+    }
+
+    public void PrintBillDetails() throws IOException, ClassNotFoundException {
+        Customer customer = Customer.read_customer(Utilities.CurrentUserID);
+        assert customer != null;
+        customer_name_2.setText(customer.username);
+        government_2.setText(customer.getRegion());
+        address_2.setText(customer.getAddress());
+        real_comsuption_p.setText(customer.getRealConsumption()+"");
+        double monthlyReading = Double.parseDouble(MonthlyRead.getText());
+        double charges = monthlyReading * chargeConstant;
+        charges_p.setText(charges + "");
+    }
+
+
 }
