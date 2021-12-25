0751bad0e11e578b462340507e276061bd438528
minazaher <menazaher115@gmail.com>
2021-12-25 12:18:02 +0200

    hell

--- src/main/java/com/user/electricity/Utilities.java
+++ src/main/java/com/user/electricity/Utilities.java
@@ -10,10 +10,12 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Properties;
 import java.util.Scanner;
+/*
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import javax.mail.Transport;
+*/
 public class Utilities {
     static String CustomersFilename = "customers.txt";
     static String OperatorsFilename = "operators.txt";
@@ -77,7 +79,7 @@ public class Utilities {
         }
         return login;
     }
-    public static void sendEmail(Customer customer, String text){
+   /* public static void sendEmail(Customer customer, String text){
         String from = "admin@gmail.com";
         String to = customer.getEmail();
         String host = "smtp.gmail.com";
@@ -113,5 +115,5 @@ public class Utilities {
 
     }
 
-
+*/
 }
